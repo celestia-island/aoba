f406aeae2db64c91e09d3122d069eccb5503a106
伊欧 <langyo.china@gmail.com>
2025-09-30 11:37:02 +0800

    💚 Remove smoke tests.

--- examples/tui_smoke_tests/src/tests/mod.rs
+++
@@ -1,7 +0,0 @@
-mod detection;
-mod startup_ctrlc;
-mod virtual_ports;
-
-pub use detection::test_tui_startup_detection;
-pub use startup_ctrlc::test_tui_startup_ctrl_c_exit;
-pub use virtual_ports::test_tui_with_virtual_ports;
